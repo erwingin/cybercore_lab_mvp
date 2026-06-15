bdd9f879dee4e983b38ce40f2faac972cbccc43e
erwingin <jnggau@gmail.com>
2026-06-15 18:26:04 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,7 @@ import sys
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR))
 
 import asyncio
 import copy
